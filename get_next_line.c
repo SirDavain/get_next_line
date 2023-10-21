4844dbc0d950fe2e8bebdeaac47df77fe67f47bc
David Ulrich <ulrichd@Davids-MacBook-Pro.local>
2023-10-21 15:36:22 +0200

    implemented my first linked list

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ulrichd <ulrichd@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 19:56:05 by dulrich           #+#    #+#             */
-/*   Updated: 2023/10/17 19:56:13 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/10/21 15:34:02 by ulrichd          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+
+char	*get_next_line(int fd)
+{
+	static t_list	*keep;
+	char			*next_line;
+
+
+
+}
--- get_next_line.h
+++ get_next_line.h
@@ -3,17 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ulrichd <ulrichd@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 19:57:06 by dulrich           #+#    #+#             */
-/*   Updated: 2023/10/17 19:58:56 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/10/21 15:33:56 by ulrichd          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <>
+# include <unistd.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
+# ifndef BUFFER_SIZE
+#	define BUFFER_SIZE 10
+# endif
+
+typedef struct	s_list
+{
+	char			*str_buf;
+	struct s_list	*next;
+} t_list;
+
+char *get_next_line(int fd);
 
 #endif
