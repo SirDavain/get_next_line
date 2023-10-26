73fd76588c7955c4c7f49319c74b425b9f7a2a0d
David Ulrich <dulrich@c2r3p5.42vienna.com>
2023-10-26 16:07:54 +0200

    updating

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 19:18:43 by dulrich           #+#    #+#             */
-/*   Updated: 2023/10/24 20:17:04 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/10/26 16:07:13 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 char	*get_next_line(int fd)
 {
+	if (fd <= 0 || BUFFER_SIZE < 0)
+		return (NULL);
 	
 }
+
+/*
+PSEUDOCODE:
+check if passed fd is valid;
+3 static vars:
+#1: stores the read() function
+#2: stores the output maybe
+#3: 
+
+If new line char is found (ft_strchr) return everything until that point and
+set the var to +1 to skip it
+Check the BUFFER_SIZE to see if there is still some left before calling read() again
+*/
