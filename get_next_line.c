89b4edabb894a9bf1b33e223e9296ad47b80b82d
David Ulrich <ulrichd@Davids-MacBook-Pro.local>
2023-10-27 22:08:59 +0200

    idea is coming together

--- get_next_line.c
+++ get_next_line.c
@@ -6,25 +6,46 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 19:18:43 by dulrich           #+#    #+#             */
-/*   Updated: 2023/10/26 16:07:13 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/10/27 22:07:34 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int	check_for_newline()
+{
+	while (the line is not \n or BUFFER_SIZE is 0)
+	{
+		put what is at the current position in the buf;
+
+	}
+	if (\n)
+		return (something);
+}
+
 char	*get_next_line(int fd)
 {
-	if (fd <= 0 || BUFFER_SIZE < 0)
+	static int		chars_read;
+	static char		*output;
+	char			*buf;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	
+	buf = malloc(BUFFER_SIZE); //BUFFER_SIZE + 1 instead?
+	chars_read = read(fd, buf, BUFFER_SIZE);
 }
 
 /*
 PSEUDOCODE:
-check if passed fd is valid;
+Process: 
+	check if passed fd is valid;
+	read until either BUFFER_SIZE is 0 or \n was found
+	if BUFFER_SIZE is 0, read again
+	if \n is found, return the line
+	if EOF return the line
 3 static vars:
 #1: stores the read() function
-#2: stores the output maybe
+#2: a char pointer stores the output
 #3: 
 
 If new line char is found (ft_strchr) return everything until that point and
